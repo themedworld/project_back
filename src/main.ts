4f7844333ce7b9e78a53aa61f4be5149986b2356
themedworld <99541159+themedworld@users.noreply.github.com>
2026-05-04 00:34:15 +0100

    add user profile and projectmarrketing task+sprint and for callcenter project task+sprint1 111

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as express from 'express';
-import * as bodyParser from 'body-parser';
 import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(bodyParser.json({ limit: '50mb' }));
-  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
   app.use(cors({
-origin: '*', 
+    origin: '*',
     credentials: true,
   }));
-  app.setGlobalPrefix('api/v1')
+
+  app.setGlobalPrefix('api/v1');
+
   const port = process.env.PORT || 3001;
   await app.listen(port);
+
+  console.log(`Server running on port ${port}`);
 }
 bootstrap();
-
