dbf21a1d0fefb383a36b7f0508c69b1292f20ccd
themedworld <99541159+themedworld@users.noreply.github.com>
2026-05-04 00:08:03 +0100

    add user profile and projectmarrketing task+sprint and for callcenter project task+sprint1 11

--- src/member-profile/member-profile.module.ts
+++ src/member-profile/member-profile.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { MemberProfileEntity } from './entities/member-profile.entity';
 import { MemberProfileService } from './member-profile.service';
 import { MemberProfileController } from './member-profile.controller';
+import { UserEntity } from 'src/user/entities/user.entity';
+
 
 @Module({
-  imports: [TypeOrmModule.forFeature([MemberProfileEntity])],
+  imports: [TypeOrmModule.forFeature([MemberProfileEntity,UserEntity])],
   providers: [MemberProfileService],
   controllers: [MemberProfileController],
   exports: [MemberProfileService],
