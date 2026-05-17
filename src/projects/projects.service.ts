d624267094638022feabb9ecbb8bc4b11a26fa76
themedworld <99541159+themedworld@users.noreply.github.com>
2026-05-17 23:11:54 +0100

    jjdjkl

--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -661,6 +661,8 @@ async getSprintsOfProjectIT(projectId: number): Promise<SprintITEntity[]> {
     const previousStatus = task.status;
 
     Object.assign(task, rest);
+  if (scheduledStartDate) task.scheduledStartDate = new Date(scheduledStartDate);
+  if (scheduledEndDate)   task.scheduledEndDate   = new Date(scheduledEndDate);
 
     if (assignedTo?.id) {
       const member = await this.userRepo.findOne({ where: { id: assignedTo.id } });
